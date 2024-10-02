25e25651faab30980d5c59139706bfa45750b431
Sofia <sofihanemann@gmail.com>
2024-10-02 12:27:02 +0000

    Calculadora 4 operações

--- src/index.mjs
+++ src/index.mjs
@@ -10,6 +10,8 @@ const numero2 = document.querySelector("#numero2");
 
 const btnCalcular = document.querySelector("#btnCalcular");
 
+const btnLimpar = document.querySelector("#btnLimpar");
+
 const resultados = document.querySelector(".resultados");
 
 const soma = document.querySelector(".soma");
@@ -25,8 +27,26 @@ const divisao = document.querySelector(".divisao");
 function calcular() {
   const n1 = Number(numero1.value);
   const n2 = Number(numero2.value);
+
   soma.innerHTML = `A soma de ${n1} e ${n2} é : ${n1 + n2}`;
+
+  subtracao.innerHTML = `A subtração de ${n1} e ${n2} é : ${n1 - n2}`;
+
+  multiplicacao.innerHTML = `A multiplicação de ${n1} e ${n2} é : ${n1 * n2}`;
+
+  divisao.innerHTML = `A divisão de ${n1} e ${n2} é : ${n1 / n2}`;
+
   //return res;
+}
+
+function limparFormulario() {
+  const n1 = Number(numero1.value);
+  const n2 = Number(numero2.value);
+  if (n1 || n2) {
+    const form = document.querySelector("form");
+    form.reset();
+    document.querySelector("#numero1").focus();
+  }
 }
 
 calcular();
@@ -36,4 +56,9 @@ calcular();
 btnCalcular.addEventListener("click", function (evento) {
   evento.preventDefault();
   calcular();
+});
+
+btnLimpar.addEventListener("click", function (evento) {
+  evento.preventDefault();
+  limparFormulario();
 });
